6a59a7848e50449e8c7dc40ba551a9cbe6d91086
Mahmoud Mansour <mansour@gmail.com>
2025-08-27 20:48:57 +0300

    Complete 3rd Video - Part 03 Non-Generic Collections ArrayList

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Demo
+﻿using System.Collections;
+
+namespace Demo
 {
     internal class Program
     {
@@ -12,7 +14,28 @@
 
             #endregion
 
+            #region Non-Generic Collections [ArrayList]
+            ArrayList arrayList = new ArrayList();
 
+            Console.WriteLine($"{arrayList.Capacity}, {arrayList.Count}");
+
+            #region Add Element
+            arrayList.Add(1); // Boxing
+            Console.WriteLine($"{arrayList.Capacity}, {arrayList.Count}");
+
+            arrayList.AddRange(new int[] { 2, 3, 4 });
+            Console.WriteLine($"{arrayList.Capacity}, {arrayList.Count}");
+
+            arrayList.Add(5); // Boxing
+            Console.WriteLine($"{arrayList.Capacity}, {arrayList.Count}");
+
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+
+            #endregion
+            #endregion
         }
     }
 }
