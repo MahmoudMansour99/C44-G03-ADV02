2de6b1e93f970018198dfec1c56187a87e85daf0
Mahmoud Mansour <mansour@gmail.com>
2025-08-27 21:12:44 +0300

    Complete 4th Video - Part 04 ArrayList Methods

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -15,27 +15,90 @@ namespace Demo
             #endregion
 
             #region Non-Generic Collections [ArrayList]
-            ArrayList arrayList = new ArrayList();
+            //ArrayList arrayList = new ArrayList();
 
-            Console.WriteLine($"{arrayList.Capacity}, {arrayList.Count}");
+            //Console.WriteLine($"{arrayList.Capacity}, {arrayList.Count}");
 
             #region Add Element
-            arrayList.Add(1); // Boxing
-            Console.WriteLine($"{arrayList.Capacity}, {arrayList.Count}");
+            //arrayList.Add(1); // Boxing
+            //Console.WriteLine($"{arrayList.Capacity}, {arrayList.Count}");
 
-            arrayList.AddRange(new int[] { 2, 3, 4 });
-            Console.WriteLine($"{arrayList.Capacity}, {arrayList.Count}");
+            //arrayList.AddRange(new int[] { 2, 3, 4 });
+            //Console.WriteLine($"{arrayList.Capacity}, {arrayList.Count}");
 
-            arrayList.Add(5); // Boxing
-            Console.WriteLine($"{arrayList.Capacity}, {arrayList.Count}");
+            //arrayList.Add(5); // Boxing
+            //Console.WriteLine($"{arrayList.Capacity}, {arrayList.Count}");
 
-            foreach (var item in arrayList)
-            {
-                Console.WriteLine(item);
-            }
+            //foreach (var item in arrayList)
+            //{
+            //    Console.WriteLine(item);
+            //}
 
             #endregion
+
+            #region Insert Element
+
+            //arrayList.Insert(1, 10); // Boxing
+            //Console.WriteLine("///////////");
+            //Console.WriteLine($"{arrayList.Capacity}, {arrayList.Count}");
+            //foreach (var item in arrayList) // Unboxing
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            ////arrayList.Insert(7, 200); // Our=t of range
+            //arrayList.InsertRange(2, new int[] { 20, 30, 40 });
+            //Console.WriteLine("///////////");
+            //Console.WriteLine($"{arrayList.Capacity}, {arrayList.Count}");
+            //foreach (var item in arrayList) 
+            //{
+            //    Console.WriteLine(item);
+            //}
+
             #endregion
+
+            #region Trim To Size
+            //Console.WriteLine($"{arrayList.Capacity}, {arrayList.Count}");
+            //arrayList.TrimToSize();
+            //Console.WriteLine($"{arrayList.Capacity}, {arrayList.Count}");
+
+            //arrayList.Add(100);
+            //Console.WriteLine($"{arrayList.Capacity}, {arrayList.Count}");
+
+            #endregion
+
+            //Console.WriteLine($"{arrayList.Capacity}, {arrayList.Count}");
+            //foreach (var item in arrayList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //arrayList.Remove(2);
+            ////arrayList.RemoveAt(5);
+            ////Console.WriteLine($"{arrayList.Capacity}, {arrayList.Count}");
+            ////foreach (var item in arrayList)
+            ////{
+            ////    Console.WriteLine(item);
+            ////}
+
+            ////int firstElement = (int?)arrayList[0] ?? 0;
+
+            //Console.WriteLine(arrayList.Contains(1));
+
+            //arrayList = new ArrayList() { 1, 2, 3, 4, 5 };
+            //Console.WriteLine($"{arrayList.Capacity}, {arrayList.Count}");
+
+            //arrayList = new ArrayList(10) { 1, 2, 3, 4, 5 };
+            //Console.WriteLine($"{arrayList.Capacity}, {arrayList.Count}");
+
+            //arrayList = new ArrayList(new int[] { 1, 2, 3, 4, 5 });
+            //Console.WriteLine($"{arrayList.Capacity}, {arrayList.Count}");
+
+            //arrayList = new ArrayList();
+
+            #endregion
+
+
         }
     }
 }
